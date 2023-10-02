8f3efb0b79a1d297dc64e41600a8a0d7ff224689
Anastasia <nastasamorgenstern64@gmail.com>
2023-10-02 15:03:24 +0300

    Added null-value for property Next by default.

--- Lab1/Node.cs
+++ Lab1/Node.cs
@@ -5,6 +5,7 @@ public class Node<T> where T : IComparable
     public Node(T data)
     {
         Data = data;
+        Next = null;
     }
     public T Data { get; set;  }
     public Node<T>? Next { get; set; }
