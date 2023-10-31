f26fa32ae58c53f5c15e1319cdaf7e9f8b45990c
Anastasia <nastasamorgenstern64@gmail.com>
2023-10-31 14:26:04 +0200

    Added MyCollectionGetIndexOfTests.

---
+++ Lab1/MyCollection.Tests/MyCollectionGetIndexOfTests.cs
@@ -0,0 +1,41 @@
+using Lab1;
+
+namespace MyCollection.Tests;
+
+[TestFixture]
+public class MyCollectionGetIndexOfTests
+{
+    [Test]
+    public void GetIndexOf_NotContainsItem_ReturnException()
+    {
+        MySortedList<int> sortedList = new MySortedList<int>();
+        Random rand = new Random();
+        
+        for (int i = 0; i < 10; i++)
+        {
+            int item = rand.Next(-10, 11);
+            sortedList.Add(item);
+        }
+        
+        Assert.Throws<Exception>(() => sortedList.GetIndexOf(rand.Next(11, 20)));
+    }
+    
+    [Test]
+    public void GetIndexOf_ContainsItem_ReturnItemIndex()
+    {
+        MySortedList<int> sortedList = new MySortedList<int>();
+        Random rand = new Random();
+        List<int> list = new List<int>();
+        
+        for (int i = 0; i < 10; i++)
+        {
+            int item = 10 - i;
+            list.Add(item);
+            sortedList.Add(item);
+        }
+        int searchedItem = rand.Next(1, 11);
+        list.Sort();
+        
+        Assert.That(sortedList.GetIndexOf(searchedItem), Is.EqualTo(list.IndexOf(searchedItem)));
+    }
+}
